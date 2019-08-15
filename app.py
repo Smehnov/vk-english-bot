36b868ec1ab932da89365461e424a067c3afe2a6
Alex <Alexander.smehnov@gmail.com>
2019-08-15 21:59:05 +0300

    Secret key varification
    Added varification by secret key

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, json
-from settings import confirmation_token
+from settings import confirmation_token,secret
 import vk, bot
 import random
 app = Flask(__name__)
@@ -14,14 +14,17 @@ def processing():
     print(data)
     if 'type' not in data.keys():
         return 'not vk'
-    if data['type']=='confirmation':
-        return confirmation_token
-    elif data['type']=='message_new':
-        user_id = data['object']['from_id']
-        user_message = data['object']['text']
-        bot.message_processing(user_id=user_id, user_message=user_message.lower())
+    if 'secret' in data.keys():
+        if data['secret']==secret:
+
+            if data['type']=='confirmation':
+                return confirmation_token
+            elif data['type']=='message_new':
+                user_id = data['object']['from_id']
+                user_message = data['object']['text']
+                bot.message_processing(user_id=user_id, user_message=user_message.lower())
         
-        return 'ok'
+                return 'ok'
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port = '80')
